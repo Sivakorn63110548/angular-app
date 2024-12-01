b6fdaff2c54b0a1294cbeccb5dc87df8b1c55b8f
BookDev <sivakorn.987@gmail.com>
2024-12-01 14:21:22 +0700

    update select leagues

--- src/app/sport/football/football.component.ts
+++ src/app/sport/football/football.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-football',
-  imports: [],
+  standalone: true,
+  imports: [
+    HttpClientModule,
+    FormsModule,
+    CommonModule,
+    MatSelectModule
+  ],
   templateUrl: './football.component.html',
-  styleUrl: './football.component.css'
+  styleUrls: ['./football.component.css']
 })
 export class FootballComponent {
+  competitions: any[] = [];
+  selectedCompetition: any;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getFilteredCompetitions();
+  }
+
+  getFilteredCompetitions(): void {
+    const url = 'http://api.football-data.org/v4/competitions';
+    const params = {
+      areas: '2072',
+      season: '2023',
+      code: 'PL'
+    };
+    const headers = { 'X-Auth-Token': '7d1ac53f18e94d93806f9dca9e5b4f28' };
+
+    this.http.get(url, { headers }).subscribe({
+      next: (data: any) => {
+        this.competitions = data.competitions.map((competition: any) => ({
+          id: competition.id,
+          name: competition.name,
+          emblem: competition.emblem || 'https://crests.football-data.org/SA.png'
+        }));
+      },
+      error: (error) => console.error('Error:', error)
+    });
+  }
+
+  onCompetitionChange() {
+    console.log(this.selectedCompetition);
+  }
+
+  getSelectedCompetition() {
+    return this.competitions.find(competition => competition.id === this.selectedCompetition);
+  }
 }
